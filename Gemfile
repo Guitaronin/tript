7d607e33e2749ba19b7c1e56d5782fd011823e6e
Arian Ryan <arian@beenverified.com>
2013-07-04 16:01:46 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+# Stack
+gem 'mysql2'
+gem 'rails', '3.2.13'
+gem 'unicorn'
+
+# Framework
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem 'therubyracer'
+
+
+# Application
+gem "cancan", ">= 1.6.8"
+gem "devise", ">= 2.1.2"
+gem 'will_paginate'
+
+
+group :deployment do
+  gem "capistrano", "2.13.5"
+  gem "rvm-capistrano"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tript::Application.config.secret_token = 'a0280085cb4a58276fce51cdd937120f816d6c69e2692b2158b8861f9a36d1340606a1477aa64c6de9a9f755b9824cb25f9a51bc569a111eab900c06bb5384b9'
