18d7061bfe572dfd2502bd0c1bbf5b9a935d67f1
Arian Ryan <arian@beenverified.com>
2013-07-15 16:30:47 -0400

    got Devise registration and auth setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts 'DEFAULT USERS'
+# user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :email => 'arianryan@gmail.com', :password => 'please', :password_confirmation => 'please'
+puts 'user: ' << user.inspect
